725a6abedb53bddf0238adbf2ef1b533b3d56279
Anthony Liang <antliang1117@gmail.com>
2022-02-09 21:48:48 -0500

    initialized minSubArrayLen.js and findLongestSubstring.js

---
+++ slidingWindow/findLongestSubstring.js
@@ -0,0 +1,3 @@
+/*
+Write a fucntion called findLongestSubstring, which accepts a string and returns the length of the longest subtring with all distinct characters
+*/
---
+++ slidingWindow/minSubArrayLen.js
@@ -0,0 +1,4 @@
+/*
+Write a function called minSubArrayLen whcih accepts two parameters - an array of postive numbers and a positive integer
+This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isnt one, return 0 instead.
+*/
