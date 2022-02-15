05c5aa568513879956062be097a28129db3cd1f0
Anthony Liang <antliang1117@gmail.com>
2022-02-14 22:10:08 -0500

    ADD: create bubble sort dir and bubble sort function

---
+++ bubbleSort/bubbleSort.js
@@ -0,0 +1,51 @@
+/*
+Restate:
+Create a function that does bubble sort given an array of numbers
+
+Examples:
+bubbleSort([4,2,6,8,19]) => [2,4,6,8,19]
+bubbleSort([10,25,13,85,93,2]) => [2,10,13,25,85,93]
+bubbleSort([9,2,1,4,65,32,42]) => [1,2,4,9,32,42,65]
+
+Approach / Code:
+create a seperate function for swapping
+add a noswap variable for optmiziation
+*/
+
+function bubbleSort(array) {
+  // create variable for whether we swapped or not
+  let noSwap;
+  // create outer loop that starts from the end to the beginning of the array
+  for (let i = array.length; i > 0; i--) {
+    // set noSwap to be true;
+    noSwap = true;
+    // create inner loop that starts from the beginning til i - 1
+    for (let j = 0; j < i - 1; j++) {
+      // if the value at the current index of the inner loop is greater than value at the next index of the inner loop, swap
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1);
+        // if swap happens, set noSwap as false
+        noSwap = false;
+      }
+    }
+
+    // if no swap has occured through the whole loop, means that the array is sorted and we can break out of bigger loop
+    if (noSwap) break;
+  }
+  // return sorted array
+  return array;
+}
+
+function swap(arr, idx1, idx2) {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+}
+
+console.log(bubbleSort([4, 2, 6, 8, 19]));
+
+/*
+Time Complexity: O(n^2)
+- since we are using a nested loop, that is using the input , input amount of times so thats quadratic time
+
+Space Complexity: O(1)
+- since most of the time we can just assigning variables with primitive types and we are not creating copies of arrays, its constant space
+*/
