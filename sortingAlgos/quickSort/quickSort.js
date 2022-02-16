bcb05e243dd123291ab35dbc70d69d18bb221a1f
Anthony Liang <antliang1117@gmail.com>
2022-02-16 16:56:47 -0500

    ADD: complete quickSort function

--- sortingAlgos/quickSort/quickSort.js
+++ sortingAlgos/quickSort/quickSort.js
@@ -17,15 +17,15 @@ function swap(array, idx1, idx2) {
   [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
 }
 
-function pivot(array) {
+function pivot(array, pivotIndex = 0, endIndex = array.length - 1) {
   // pick a pivot (the index) that we will use to determine where everything else in the array should be, for now it will be first element
-  let pivotIndex = 0;
+  // we initialzed in the arguments
 
-  // create a counter that tracks how many numbers are less than the pivot
-  let counter = 0;
+  // create a counter that tracks how many numbers are less than the pivot starting from the pivotidx
+  let counter = pivotIndex;
 
-  // loop through array
-  for (let i = 0; i < array.length; i++) {
+  // loop through array, we can start at 1 since we already using the first element as the pivot element
+  for (let i = pivotIndex + 1; i <= endIndex; i++) {
     // based on the value of the pivot
     let currentElem = array[i];
     let pivotElem = array[pivotIndex];
@@ -37,7 +37,7 @@ function pivot(array) {
       counter++;
 
       // swap the values of the current element and the value to the right of the pivot or to right of the most recently swapped element
-      swap(array, i, pivotIndex + counter);
+      swap(array, i, counter);
     }
   }
   // once we are out of the loop, we will use the counter and swap places with the pivot and the counter
@@ -47,4 +47,25 @@ function pivot(array) {
   return counter;
 }
 
-console.log(pivot([5, 2, 1, 8, 4, 7, 6, 3]));
+function quickSort(array, left = 0, right = array.length - 1) {
+  // create base case that checks when the left pointer and right pointer of the sub array are equal to each other or if the right is less than the left
+  if (left < right) {
+    // calls the pivot helper function and returns that index
+    let pivotIdx = pivot(array, left, right);
+
+    // recusrively quickSort on both the left side of the array and the right side
+    quickSort(array, left, pivotIdx - 1);
+    quickSort(array, pivotIdx + 1, right);
+  }
+  // return sorted array in the end
+  return array;
+}
+
+console.log(quickSort([5, 2, 1, 8, 4, 7, 6, 3]));
+
+/*
+Time complexity: O(n^2)
+- since depending on the input, we need to do n number fo decompostions and per decomposition, we have to make a comparsion n number of times
+- O(n * n) => O(n^2)
+
+*/
