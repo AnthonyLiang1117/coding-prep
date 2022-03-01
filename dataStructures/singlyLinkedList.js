5ffc5c3e5a535021c5bc6400ca1238406812f4bd
Anthony Liang <antliang1117@gmail.com>
2022-03-01 18:34:53 -0500

    ADD: implmenet push method for singly linked list

--- dataStructures/singlyLinkedList.js
+++ dataStructures/singlyLinkedList.js
@@ -25,4 +25,36 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  // push method that takes a value and adds a node to the back of the singly linked list
+  push(value) {
+    // creates a node with the given value
+    let newNode = new Node(value);
+
+    // checks to see if there is a head node already
+    // if not,
+    if (!this.head) {
+      // assign head and tail as the new node that was created
+      this.head = newNode;
+      this.tail = this.head;
+
+      // if yes,
+    } else {
+      // assign the current tail node's next property to be the new node that was created
+      this.tail.next = newNode;
+
+      // reassign tail node as the new node that was created
+      this.tail = newNode;
+    }
+    // increment length counter by 1
+    this.length++;
+    return this;
+  }
 }
+
+const list = new SinglyLinkedList();
+list.push('good morning');
+console.log('CURRENT SINGLY LINKED LIST !!!!', list.push('good afternoon'));
+console.log('REMOVED NODE VALUE!!!', list.pop());
+console.log('REMOVED NODE VALUE!!!', list.pop());
+console.log('REMOVED NODE VALUE!!!', list.pop());
