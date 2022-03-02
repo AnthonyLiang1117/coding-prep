389c2a2d83c17ae74eadd0e85dbc28f81bcb7175
Anthony Liang <antliang1117@gmail.com>
2022-03-02 16:52:04 -0500

    ADD: shift method for singly linked list

--- dataStructures/singlyLinkedList.js
+++ dataStructures/singlyLinkedList.js
@@ -51,7 +51,7 @@ class SinglyLinkedList {
     return this;
   }
 
-  // pop method that removes the last node from the back of the singly linked list and returns the value of the node removed
+  // pop method that removes the a node from the back of the singly linked list and returns the value of the node removed
   pop() {
     // if the singly linked list is empty, return undefined
     if (!this.head) return undefined;
@@ -77,6 +77,7 @@ class SinglyLinkedList {
     // decrement the length by 1;
     this.length--;
 
+    // if we pop until the singly linked list is empty, we want to reassign the head and tail properties to represent that also
     if (this.length === 0) {
       this.head = null;
       this.tail = null;
@@ -85,11 +86,30 @@ class SinglyLinkedList {
     // return the value of the node removed
     return nextNode.value;
   }
+
+  // shift method that removes the a node from the front of the singly linked list and retursn the value of the node removed
+  shift() {
+    // if the linked list is empty, return undefined
+    if (!this.head) return undefined;
+
+    // create a variable to store the current head value
+    let removeNode = this.head;
+
+    // reassign the head property to be the current head nodes next property
+    this.head = removeNode.next;
+
+    // decrement the linked list's length property by 1;
+    this.length--;
+
+    // if we shift until the singly linked list is empty, we want to reassign the tial property to represent that
+    if (this.length === 0) this.tail = null;
+
+    // return the removed node's v
+    return removeNode.value;
+  }
 }
 
 const list = new SinglyLinkedList();
 list.push('good morning');
 console.log('CURRENT SINGLY LINKED LIST !!!!', list.push('good afternoon'));
-console.log('REMOVED NODE VALUE!!!', list.pop());
-console.log('REMOVED NODE VALUE!!!', list.pop());
-console.log('REMOVED NODE VALUE!!!', list.pop());
+console.log('REMOVED NODE VALUE!!!', list.shift());
