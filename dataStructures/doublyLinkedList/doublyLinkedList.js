11fb3b9480842a3679196a76ee120e8e5e4b58ed
Anthony Liang <antliang1117@gmail.com>
2022-03-08 19:36:22 -0500

    ADD: push method for doubly linked list

--- dataStructures/doublyLinkedList/doublyLinkedList.js
+++ dataStructures/doublyLinkedList/doublyLinkedList.js
@@ -10,7 +10,7 @@ class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
-    this.tail = null;
+    this.previous = null;
   }
 }
 
@@ -28,4 +28,39 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  // push method, takes a value, adds a node to the end of the doubly linked list
+  push(value) {
+    // create a node with the given value
+    let newNode = new Node(value);
+
+    // checks to see if the doubly linked list is empty, if it is
+    if (!this.head) {
+      // assign the head and tail properties of the doubly linked list as the newly created node
+      this.head = newNode;
+      this.tail = this.head;
+
+      // if not,
+    } else {
+      // assign the current tail property's next property to be the newly created node
+      this.tail.next = newNode;
+
+      // assign the newly created node's previous property to be the current tail node
+      newNode.previous = this.tail;
+
+      // reassign the doubly linked list's tail property to be the newly created node
+      this.tail = newNode;
+    }
+    // increment the doubly linked list's length by 1 since we added a new node to the list
+    this.length++;
+
+    // return the list
+    return this;
+  }
 }
+
+const list = new DoublyLinkedList();
+list.push('first node');
+list.push('second node');
+list.push('third node');
+console.log(list.pop());
