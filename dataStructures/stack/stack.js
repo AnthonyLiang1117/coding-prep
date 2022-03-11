6d20b8471db137ad92454e1046838a13c68fe570
Anthony Liang <antliang1117@gmail.com>
2022-03-11 16:30:23 -0500

    ADD: Node and Stack classes for stack.js

---
+++ dataStructures/stack/stack.js
@@ -0,0 +1,20 @@
+/*
+
+Implementing a stack using the principles of a singly linked list
+
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
