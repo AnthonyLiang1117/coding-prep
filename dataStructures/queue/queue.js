71961cc794c9683297b5cbf33165a69e3adead2f
Anthony Liang <antliang1117@gmail.com>
2022-03-12 14:42:31 -0500

    ADD: enqueue method for queue.js

--- dataStructures/queue/queue.js
+++ dataStructures/queue/queue.js
@@ -1,6 +1,18 @@
 /*
 
 Implementing a Queue with the principles of the singly linked list
+Needs to satisfy the First in First out rule
+
+enqueue method, takes a value, adds a node to the end of the queue
+creates a node with the given value
+checks to see if the queue is current empty
+if so,
+assign both the first and last properties to be the new node
+if not,
+point the current last's next property to be the new node
+reassign the new node to be the new tail
+increment the size by 1 since we just added a node to the queue
+return the list
 
 
 */
@@ -18,4 +30,23 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
+
+  enqueue(value) {
+    let newNode = new Node(value);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+    return this;
+  }
 }
+
+let queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
