80643495d25251e62fa7edb22ed3bc5a549f0d41
Anthony Liang <antliang1117@gmail.com>
2022-03-12 14:34:24 -0500

    ADD: initializing node and queue classes in queue.js

--- dataStructures/queue/queue.js
+++ dataStructures/queue/queue.js
@@ -2,4 +2,20 @@
 
 Implementing a Queue with the principles of the singly linked list
 
+
 */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
