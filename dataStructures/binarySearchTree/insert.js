0cf4b1f11ece9fc4678b8762beae6e90408e1ad3
Anthony Liang <antliang1117@gmail.com>
2022-03-16 16:05:24 -0400

    ADD: initalizing practice insert.js in BST dir

---
+++ dataStructures/binarySearchTree/insert.js
@@ -0,0 +1,49 @@
+/*
+
+Implementing insert method into binary search tree class
+
+Approach:
+The method should take a value and insert a node into the BST within the BST rules and return the entire list if complete
+the values that are less than the root value are to the left of it and the values that are more than the root value are to the right of it
+
+- creating a node with the given value
+- check to see if the binary search tree is empty,
+- if so,
+  - assign the new node to be the root of the BST
+  - return the BST
+- create a variable to hold the node that we are current looking at and comparing with the new node we created
+- create a while loop that keeps going
+  - check to see if the value of the new node is equal to, less than or greater than the value of the current node we are looking at
+  - if they are equal,
+    - return undefined
+  - if less than,
+    - check to see if there is a left child node already for the current node
+    - if not,
+      - assign the new node to be the left child
+      - return the BST to break out of the while loop
+    - if there is,
+      - reassign the current node we are looking at to be the left child
+      - redo these steps within the while loop
+  - if more than,
+    - check to see if there is a right child for the current node
+    - if not,
+      - assign the new node to be the right child
+      - return the list
+    - if yes,
+      - reassign the current node we are looking at to be the right child
+      - redo the steps within the while loop
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
