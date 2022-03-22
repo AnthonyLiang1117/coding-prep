58a5f894062ecf478e2dbbb6dde6ae14ee050dc6
Anthony Liang <antliang1117@gmail.com>
2022-03-22 17:28:31 -0400

    ADD: dequeue method for priority queue.js

--- dataStructures/binaryHeap/priorityQueue.js
+++ dataStructures/binaryHeap/priorityQueue.js
@@ -31,6 +31,39 @@ bubbleUp method
   - swap the values of element and parent
   - reassign the index to be the parent
 
+dequeue method
+- grab top node from the priority queue
+- get the end node from the priority queue by popping it off
+- if there is more than 0 nodes in the priority queue
+  - reassign the value at the top node to be the end node
+  - invoke the sinkDown method
+- return the top node
+
+sinkDown method
+- grabs the index of the newly assigned top node
+- grabs the value at that index
+- creates a variable to represent the length of the priority queue
+- initializes the left and right children values variables
+- initalizes a swap variable to represent if we swapped or not during the iteration
+- creates while loop that runs true and we will eventually break out of it using break
+  - grabs the index of the left and right children based off of the index of the newly assigned top node
+  - checks to see if the left child index is within bounds of the priority queue
+    - if so,
+    - assign left child to have the value at the left child index
+    - checks if the left child's priority is less than the priorty of the parent
+      - if so,
+      - assign swap to be left child index
+  - checks to see if the right child index is within bounds of the priority queue
+    - if so,
+    - assign right child to have the value at the right child index
+    - check to see if we have not swapped yet and if the right child's priority is less than the priority parent's
+      OR if we have swapped and if the right child's priority is less than the left child's priority
+      - if so,
+      - assign swap to be right child index
+  - if we did not swap at all during this iteration, we can break out of the loop since the parent is at the right spot
+  - swap the values of index and swap
+  - reassign index to be swap
+
 */
 
 class Node {
@@ -66,6 +99,58 @@ class PriorityQueue {
       this.values[index] = parent;
       this.values[parent] = element;
       index = parentIndex;
+    }
+  }
+
+  dequeue() {
+    let min = this.values[0];
+    let end = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+
+    return min;
+  }
+
+  sinkDown() {
+    let index = 0;
+    const node = this.values[index];
+    const length = this.values.length;
+
+    while (true) {
+      let leftChild;
+      let rightChild;
+      let swap = null;
+
+      let leftIndex = 2 * index + 1;
+      let rightIndex = 2 * index + 2;
+
+      if (leftIndex < length) {
+        leftChild = this.values[leftIndex];
+
+        if (leftChild.priority < node.priority) {
+          swap = leftIndex;
+        }
+      }
+
+      if (rightIndex < length) {
+        rightChild = this.values[rightIndex];
+
+        if (
+          (swap === null && rightChild.priority < node.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightIndex;
+        }
+      }
+
+      if (swap === null) break;
+
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
     }
   }
 }
