1654a1b84c1a527d979589b75e207576410ca959
Anthony Liang <antliang1117@gmail.com>
2022-03-29 16:27:36 -0400

    ADD: removeEdge method for graph class

--- dataStructures/graphs/undirectedGraph.js
+++ dataStructures/graphs/undirectedGraph.js
@@ -27,8 +27,33 @@ class Graph {
       this.adjacencyList[name] = [];
     }
   }
+
+  // addEdge method which accepts 2 vertices and adds the connection into each of the arrays at the 2 keys
+  addEdge(vertex1, vertex2) {
+    // pushes the name of vertex 2 onto the array list at the key that represents vertex 1
+    this.adjacencyList[vertex1].push(vertex2);
+
+    // pushes the name of vertex 1 onto the array list at the key that represents vertex 2
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  // removeEdge method accepts 2 vertices and removes the connection between the 2 vertices
+  removeEdge(vertex1, vertex2) {
+    // reassign the connections array at vertex1 to include everything but vertex 2 in it
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (vertex) => vertex !== vertex2
+    );
+
+    // reassign the connections array at vertex2 to include everything but vertex 1 in it
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (vertex) => vertex !== vertex1
+    );
+  }
 }
 
 const graph = new Graph();
-graph.addVertex('HELLO');
-console.log(graph.adjacencyList);
+graph.addVertex('New York');
+graph.addVertex('Los Angeles');
+graph.addVertex('Chicago');
+graph.addEdge('New York', 'Los Angeles');
+console.log(graph);
