a0c1bfc063aadce93e5a865bc72a2732fb7aa158
Anthony Liang <antliang1117@gmail.com>
2022-03-29 16:06:16 -0400

    ADD: addVertex method for graph class

--- dataStructures/graphs/undirectedGraph.js
+++ dataStructures/graphs/undirectedGraph.js
@@ -14,4 +14,21 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  // addVertex method which accepts a name of the vertex and adds it to the graph as a key in the adjacency list
+  addVertex(name) {
+    // checks to see if there is already a key with the same name in the graph
+    // if not,
+
+    if (!this.adjacencyList.hasOwnProperty(name)) {
+      // assign the adjacencyList property a key based off the given name to represent a vertex
+      // with a value of an array to store the connection pairs it will have
+
+      this.adjacencyList[name] = [];
+    }
+  }
 }
+
+const graph = new Graph();
+graph.addVertex('HELLO');
+console.log(graph.adjacencyList);
