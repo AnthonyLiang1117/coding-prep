dc19ae2830405e0338b7af2a5a274a2336fc02db
Anthony Liang <antliang1117@gmail.com>
2022-03-29 15:52:22 -0400

    ADD: initialize graph class in graphs dir

---
+++ dataStructures/graphs/undirectedGraph.js
@@ -0,0 +1,17 @@
+/*
+
+Implementing a undirected graph class and storing it using an adjacency list
+
+Graph class
+- a graph is a collection of nodes that have connections between the nodes in no particular order
+- has an adjacency list property that initializes as an object to later store the key value pairs
+- keys will be the vertices
+- values will be the connections it has from that vertex
+
+*/
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+}
