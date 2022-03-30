d3fcfafd83c2800bf7952cb315467a64b3fa6778
Anthony Liang <antliang1117@gmail.com>
2022-03-30 14:21:55 -0400

    ADD: practice initializing graph class

---
+++ dataStructures/graphs/practiceGraph.js
@@ -0,0 +1,19 @@
+/*
+
+Implementing a graph class
+- graphs are a bunch of vertices (nodes) connected with edges (connections)
+- we will be implementing a undirected unweighted graph and storing it in an adjacency list
+- has an adjacencyList property that will be initialized as an js object
+
+addVertex
+addEdge
+removeEdge
+removeVertex
+
+*/
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+}
