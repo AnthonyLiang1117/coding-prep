22f29580c378fe41e4f96a96b5e67ad09a8648f1
Anthony Liang <antliang1117@gmail.com>
2022-04-07 18:00:29 -0400

    ADD: initialized dijkstra's algo into weighted graph class

--- dataStructures/graphs/weightedGraph.js
+++ dataStructures/graphs/weightedGraph.js
@@ -26,6 +26,44 @@ class WeightedGraph {
     this.adjacencyList[vertex1].push({ node: vertex2, weight });
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
+
+  // dijkstra accept 2 vertices and returns the shorest path in an array
+  dijkstra(start, end) {
+    // initializing a priorty queue
+    const PQ = new PriorityQueue();
+
+    // initializing a distance obj that will take in account how much distance interms of weight is the start vertex to any vertex in the graph
+    const distance = {};
+
+    // initializing a previous obj that will take in account at each vertex, which vertex is it traveling from
+    const previous = {};
+
+    // create a for in loop with the keys of the adjacency list
+    for (let vertex in this.adjacencyList) {
+      // if the current key we are at is the starting vertex
+      if (vertex === start) {
+        // create a key in the distance obj with a value of 0
+        distance[vertex] = 0;
+
+        // enqueue onto the PQ with the vertex and the weight aka 0
+        PQ.enqueue(vertex, 0);
+
+        // if not,
+      } else {
+        // create a key in the in the distance obj with a value of infinity
+        distance[vertex] = Infinity;
+
+        // enqueue onto the PQ with the vertex and the weight aka infinity
+        PQ.enqueue(vertex, Infinity);
+      }
+
+      // create a key in the previous obj for every iteration with a value of null
+      previous[vertex] = null;
+    }
+
+    console.log(distance);
+    console.log(previous);
+  }
 }
 
 /*
@@ -69,8 +107,16 @@ const WG = new WeightedGraph();
 WG.addVertex('A');
 WG.addVertex('B');
 WG.addVertex('C');
-WG.addEdge('A', 'B', 9);
-WG.addEdge('A', 'C', 5);
-WG.addEdge('B', 'C', 7);
+WG.addVertex('D');
+WG.addVertex('E');
+WG.addVertex('F');
 
-console.log(WG.adjacencyList);
+WG.addEdge('A', 'B', 4);
+WG.addEdge('A', 'C', 2);
+WG.addEdge('B', 'E', 3);
+WG.addEdge('C', 'D', 2);
+WG.addEdge('D', 'E', 3);
+WG.addEdge('D', 'F', 1);
+WG.addEdge('E', 'F', 1);
+
+console.log(WG.dijkstra('A', 'E'));
