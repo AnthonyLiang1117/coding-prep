d8bc0c2ae830b4116aac59121f34de13bec4a596
Anthony Liang <antliang1117@gmail.com>
2022-04-11 15:07:58 -0400

    ADD : optimizied solution for countZero.js

--- algoApproaches/divideAndConquer/notComplete/countZeros.js
+++ algoApproaches/divideAndConquer/notComplete/countZeros.js
@@ -44,10 +44,68 @@ function countZero(array) {
 }
 
 Time Complexity: O(n)
-- since we have a for loop that will run depending on how big the input is, will give it a linear time
+- since we have a for loop that will run depending on how big the input is, will give it a linear time O(n)
+- we are accessing values from an array which is constant time O(1)
+- we are doing basic operations like + , - and = so that is constant tiem also O(1)
 
 Space Complexity: O(1)
-- since we are only creating variables that have a primitive data type like a number, it will be constant time
+- since we are only creating variables that have a primitive data type like a number, it will be constant time O(1)
+*/
+
+/*
+
+Optimized Approach:
+Since the array is sorted, we can use divide and conquer
+where we will keep halfing the array and moving the midpoint
+until we find the number that has a left number of 1
+since that will signal when the zeros start happening
+
+*/
+
+function countZero(array) {
+  // create 2 pointers that will represent the index of the beginning and the end
+  let leftPointer = 0;
+  let rightPointer = array.length - 1;
+
+  // have a while loop that will check for when the left pointer is less than or equal to the right pointer
+  while (leftPointer <= rightPointer) {
+    // calculate the index of the midpoint
+    let midpoint = Math.floor((leftPointer + rightPointer) / 2);
+
+    // check if the value of the midpoint is equal to 0 && the value at the index to the left of the midpoint is equal to 1
+    // if it is checks out,
+    if (
+      (array[midpoint] === 0 && array[midpoint - 1] === 1) ||
+      midpoint === 0
+    ) {
+      // return the length of the array subtracted by the index of the midpoint since that will let us know how many 0s there are
+      return array.length - midpoint;
+    }
+
+    // if value of the midpoint equal 0,
+    if (array[midpoint] === 0) {
+      // we know the number will be something to the left of it so we move the right pointer to be the midpoint - 1 to shrink area
+      rightPointer = midpoint - 1;
+
+      // else if the value of midpoint equals  1,
+    } else if (array[midpoint] === 1) {
+      // we know the number will be something to the right of it so we move the left pointer to be hte midpoint + 1 to shrink area we are looking for
+      leftPointer = midpoint + 1;
+    }
+  }
+  // if we break out of the loop and there is nothing returned,
+  // return 0 for 0 zeros
+  return 0;
+}
+
+/*
+Time Complexity: O(log n)
+- since we are dividing our input by half everything time we are running the loop, it is logarithmic time O(log n)
+- we are accessing values from an array which is constant time O(1)
+- we are doing basic operations like + , - and = so that is constant time also O(1)
+
+Space Complexity: O(1)
+- since we are only creating primitive data type variables, that is constant space O(1)
 */
 
 console.log(countZero([1, 1, 1]));
