9c39d4d3707f87a4c9a008cf7faeba08adfe1da8
Anthony Liang <antliang1117@gmail.com>
2022-04-15 15:35:22 -0400

    ADD: BFS method for BST

--- dataStructures/binarySearchTree/BST.js
+++ dataStructures/binarySearchTree/BST.js
@@ -53,6 +53,35 @@ find()
 - if so, reassign the current node to be the current node's right child
 - Time complexity O(log n) since we have to loop through the BST but we divide the amount of nodes in half as we loop through
 
+BFS()
+- this function should search through each node in the binary search tree using breadth first searc and reutrn an array containing each node's value
+- we want to process all nodes on the same level first before we move on to the next level
+- we do this by using a queue to remember which nodes we still need to process
+- create an array to hold the values of the nodes we need
+- create a queue to see which nodes processes first
+- initially push the root node onto the queue
+- have a while loop that checks for if theres still items in the queue
+- pop off a node of the queue
+- if the node has a left child, push it onto the queue
+- if the node has a right child, push it onto the queue
+- push the value of the node onto the return array
+- return the return array
+- Time Complexity is linear time O(n) since we will be looping through entire BST based on the number of nodes in the BST
+- Space Complexity is linear space O(n) since we will have an array based on how many nodes at in the BST
+
+DFSPreOrder ()
+- this function should search through each node in the binary search tree using pre-order depth first search and return an array containging each node's value
+- with pre order, you want to process itself first, then its left side then its right side
+- will be using a traverse helper function to traverse through BST
+- so we want to have an array that stores the values of the BST
+- create a varaible to represent the current node we are looking at , which starts at the root
+- craete helper function that accepts a node
+- since we want to process itself first, we push the nodes value onto the values array
+- sicne we want to process left side next, if they have a left child, we call the traverse helper function with the left child
+- since we want to process right side last, if they have a right child, we call the traverse helper fucntion with the right child
+- invoke the helper function with the current node
+- return the values array
+
 */
 
 class Node {
@@ -118,6 +147,26 @@ class BinarySearchTree {
       }
     }
   }
+
+  BFS() {
+    const values = [];
+
+    const queue = [];
+
+    queue.push(this.root);
+
+    while (queue.length) {
+      const currentNode = queue.shift();
+
+      if (currentNode.left) queue.push(currentNode.left);
+
+      if (currentNode.right) queue.push(currentNode.right);
+
+      values.push(currentNode.value);
+    }
+
+    return values;
+  }
 }
 
 const BST = new BinarySearchTree();
@@ -125,4 +174,8 @@ BST.insert(15);
 BST.insert(20);
 BST.insert(10);
 BST.insert(12);
-console.log(BST.find(43));
+BST.insert(1);
+BST.insert(5);
+BST.insert(50);
+
+console.log(BST.BFS());
